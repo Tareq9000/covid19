f66690918873a32e95a56eeb59fe74c3d31ff986
Tareq9000 <tkattit2@gmail.com>
2020-05-15 18:17:58 +0200

    showing up the global data

--- covid19/src/reducers/covidReducer.js
+++ covid19/src/reducers/covidReducer.js
@@ -1,15 +1,33 @@
 import { fetchAPI } from '../fetchAPI';
 
-export const getGlobalSummary = () => {
-    return ( dispatch ) => {
-      fetchAPI('https://api.covid19api.com/summary').then(fetchData => {
+const initialState = {
+  global: {}
+}
 
-        dispatch({
-          type : 'SET_GLOBAL',
-          payload : {
-            Global : fetchData[0].Global
-          }
-        })
-      })
-    }
+const covidReducer = (state = initialState, action) => {
+
+  switch(action.type){
+    case 'SET_GLOBAL':
+      return {
+        ...state,
+        global: action.payload.global
+      }
+    default:
+      return state
   }
+}
+export default covidReducer
+
+export const getGlobalSummary = () => {
+  return ( dispatch ) => {
+    fetchAPI('https://api.covid19api.com/summary').then(fetchData => {
+
+      dispatch({
+        type : 'SET_GLOBAL',
+        payload : {
+          global : fetchData[0].Global
+        }
+      })
+    })
+  }
+}
