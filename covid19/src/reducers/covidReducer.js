e89d78d5d9e6abc09e9150c673b7758b15d454b5
Tareq9000 <tkattit2@gmail.com>
2020-06-12 15:47:15 +0200

    adding a component showing the top data (confirmed, deaths, recovered)

--- covid19/src/reducers/covidReducer.js
+++ covid19/src/reducers/covidReducer.js
@@ -5,13 +5,23 @@ const initialState = {
   countries: [],
   country: "",
   spinning: false,
-  showError: false
+  showError: false,
+  topData: {
+    confirmed: {},
+    deaths: {},
+    recovered: {}
+  }
 }
 
 const covidReducer = (state = initialState, action) => {
 
   switch(action.type){
     case 'SET_GLOBAL_COUNTRY_DATA':
+      const totalNumbers = {
+        totalConfirmed: action.payload.countries.map(country => country.TotalConfirmed),
+        totalDeaths: action.payload.countries.map(country => country.TotalDeaths),
+        totalRecovered: action.payload.countries.map(country => country.TotalRecovered)
+      }
       return {
         ...state,
         global: action.payload.global,
@@ -25,6 +35,17 @@ const covidReducer = (state = initialState, action) => {
                       ...action.payload.countries 
                    ],
         country: "all countries",
+        topData: {
+          confirmed:  action.payload.countries.filter((country) => (
+                        country.TotalConfirmed === Math.max(...totalNumbers.totalConfirmed)
+                      ))[0],
+          deaths:      action.payload.countries.filter((country) => (
+                        country.TotalDeaths === Math.max(...totalNumbers.totalDeaths)
+                      ))[0],
+          recovered:  action.payload.countries.filter((country) => (
+                        country.TotalRecovered === Math.max(...totalNumbers.totalRecovered)
+                      ))[0]
+        },
         showError: false
       }
     case 'SET_SPINNER':
@@ -71,8 +92,6 @@ export const getGlobalAndCountriesData = () => {
     fetchAPI('https://api.covid19api.com/summary').then(fetchData => {
 
       if(fetchData[0]){
-        console.log(fetchData[0].Global, "GLOBAL")
-        console.log(fetchData[0].Countries, "COUNTRYS")
         dispatch({
           type : 'SET_GLOBAL_COUNTRY_DATA',
           payload : {
